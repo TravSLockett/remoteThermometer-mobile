63e4624440b57ebc73e8beb476028f9eb90ea190
Mike <mmellone99@gmail.com>
2020-06-13 11:00:16 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+
+import { StackNavigation } from "react-navigation";
 
 export default function App() {
+  const [name, setName] = useState("Mike");
+  const [age, setAge] = useState("20");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter Your Name Here"
+        onChangeText={(val) => setName(val)}
+      />
+      <Text style={styles.fontStyle}>Name: {name}</Text>
+      <TextInput
+        keyboardType="numeric"
+        style={styles.input}
+        placeholder="Enter Your Age Here"
+        onChangeText={(val) => setAge(val)}
+      />
+      <Text style={styles.fontStyle}>Age: {age}</Text>
     </View>
   );
 }
@@ -12,8 +29,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  fontStyle: {
+    fontSize: 20,
+    color: "dodgerblue",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#777",
+    padding: 8,
+    margin: 10,
+    width: 200,
   },
 });
